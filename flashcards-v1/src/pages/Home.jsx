7a681b0c050c1d9e88026c24cc49024cc6895ac7
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-07-10 22:26:41 -0300

    Desenvolvendo componente 'RadioButton':
    criando comportamento de sincronização do radioInput

--- flashcards-v1/src/pages/Home.jsx
+++ flashcards-v1/src/pages/Home.jsx
@@ -8,30 +8,65 @@ import Container from '../components/Container';
 import Botao from '../components/Botao';
 import { embaralhaArray } from '../helpers/embaralhaArray';
 import Rodape from '../components/Rodape';
+import RadioButton from '../components/RadioButton';
 
 export default function Home() {
   const [cartoes, setCartoes] = useState(listaDeCartoes);
+  const [mostraTitulo, setMostraTitulo] = useState(true);
 
   function ativaBotao() {
     const cartoesEmbaralhados = embaralhaArray(cartoes);
     setCartoes(cartoesEmbaralhados);
   }
+
+  function cliqueMostraTitulo() {
+    setMostraTitulo(true);
+  }
+
+  function cliqueMostraDescricao() {
+    setMostraTitulo(false);
+  }
+
   return (
     <div>
       <Cabecalho>Flashcards v1</Cabecalho>
+
       <Principal>
         <div className="text-center mb-4">
           <Botao onClickBotao={ativaBotao}>Embaralhar Cartões</Botao>
         </div>
+        <div
+          className={`flex flex-row items-center justify-center
+        space-x-4 m-4`}
+        >
+          <RadioButton
+            id="mostraTitulo"
+            name="mostraInformacao"
+            radioChecked={mostraTitulo}
+            clickRadioButton={cliqueMostraTitulo}
+          >
+            Mostrar Título
+          </RadioButton>
+          <RadioButton
+            id="mostraDescricao"
+            name="mostraInformacao"
+            radioChecked={!mostraTitulo}
+            clickRadioButton={cliqueMostraDescricao}
+          >
+            Mostrar Descrição
+          </RadioButton>
+        </div>
+
         <Container>
           {cartoes.map(({ id, titulo, descricao }) => {
             return <Cartao key={id} titulo={titulo} descricao={descricao} />;
           })}
         </Container>
       </Principal>
+
       <Rodape>Desenvolvido por &copy;Maria Cláudia</Rodape>
     </div>
   );
 }
 
-// continuar da aula 4.5
+// continuar da aula 4.6
