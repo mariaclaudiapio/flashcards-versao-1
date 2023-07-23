b83dbbf4c3dbff3f92fb96b42d11d94eb4d5df4c
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-07-23 20:31:41 -0300

    creating FlashcardsContainer component

---
+++ src/components/FlashcardsContainer.jsx
@@ -0,0 +1,10 @@
+export default function FlashcardsContainer({ children: flashcards }) {
+  return (
+    <div
+      className={` p-4 flex flex-row rounded-lg
+  items-center justify-center flex-wrap bg-yellow-100`}
+    >
+      {flashcards}
+    </div>
+  );
+}
--- src/pages/FlashcardsPage.jsx
+++ src/pages/FlashcardsPage.jsx
@@ -1,13 +1,18 @@
 import FlashCard from '../components/Flashcard';
+import FlashcardsContainer from '../components/FlashcardsContainer';
 import Header from '../components/Header';
 import Main from '../components/Main';
+
+import { allFlashcards } from '../data/allFlashcards';
 
 export default function FlashcardsPage() {
   return (
     <>
       <Header>React Flashcards v1</Header>
       <Main>
-        <FlashCard />
+        <FlashcardsContainer>
+          <FlashCard />
+        </FlashcardsContainer>
       </Main>
     </>
   );
